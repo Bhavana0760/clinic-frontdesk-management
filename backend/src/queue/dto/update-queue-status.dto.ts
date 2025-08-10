41b69302d78bf176969c9fcbd58b1bb38d7cecc4
Bhavana <124126826+Bhavana0760@users.noreply.github.com>
2025-08-10 17:30:42 +0530

    queue status issue fixed

--- backend/src/queue/dto/update-queue-status.dto.ts
+++ backend/src/queue/dto/update-queue-status.dto.ts
@@ -1,7 +1,20 @@
-import { IsEnum } from "class-validator"
+import { IsString, Validate, ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments } from "class-validator"
+import { Expose } from "class-transformer"
 import { QueueStatus } from "../../entities/enums"
 
+@ValidatorConstraint({ name: "isQueueStatus", async: false })
+class IsQueueStatusConstraint implements ValidatorConstraintInterface {
+  validate(status: any, _args: ValidationArguments) {
+    return Object.values(QueueStatus).includes(status)
+  }
+  defaultMessage(_args: ValidationArguments) {
+    return "Status must be a valid queue status."
+  }
+}
+
 export class UpdateQueueStatusDto {
-  @IsEnum(QueueStatus)
-  status!: QueueStatus
+  @IsString()
+  @Validate(IsQueueStatusConstraint)
+  @Expose()
+  status!: string
 }
